b9e714a7de48fafcc46a7ebd908b55d764c53acf
Syimyk <syimyk.nurdinov@axelkg.com>
2026-05-13 11:51:05 +0600

    add new materials

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -4,20 +4,104 @@
 //
 //  Created by Syimyk on 29/4/26.
 //
-
 import SwiftUI
 
 struct ContentView: View {
+//    var body: some View {
+//        Button("Hello, world!") {
+//            print(type(of: self.body))
+//        }
+//        .frame(width: 200, height: 200)
+//        .background(.red)
+        
+//        Text("Hello, world!")
+//            .padding()
+//            .background(.red)
+//            .padding()
+//            .background(.blue)
+//            .padding()
+//            .background(.green)
+//            .padding()
+//            .background(.yellow)
+        
+//        VStack {
+//            Text("Gryffindor")
+//            Text("Hufflepuff")
+//            Text("Ravenclaw")
+//            Text("Slytherin")
+//        }
+//        .font(.title)
+        
+//        VStack {
+//            Text("Gryffindor")
+//                .font(.largeTitle)
+//            Text("Hufflepuff")
+//            Text("Ravenclaw")
+//            Text("Slytherin")
+//        }
+//        .font(.title)
+        
+//        VStack {
+//            Text("Gryffindor")
+////                .blur(radius: 0)
+//            Text("Hufflepuff")
+//            Text("Ravenclaw")
+//            Text("Slytherin")
+//        }
+//        .blur(radius: 5)
+        
+        
+//    }
+    
+    let motto1 = Text("Draco dormiens")
+    let motto2 = Text("nunquam titillandus")
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-            .background(.red)
+//        VStack {
+//            motto1
+//                .foregroundStyle(.red)
+//            motto2
+//                .foregroundStyle(.blue)
+//        }
+        Text("Hello World")
+            .modifier(Title())
+        
+        Color.blue
+            .frame(width: 300, height: 200)
+            .watermarked(with: "Hacking with Swift")
+    }
+
+}
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundStyle(.white)
             .padding()
             .background(.blue)
-            .padding()
-            .background(.green)
-            .padding()
-            .background(.yellow)
+            .clipShape(.rect(cornerRadius: 10))
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundStyle(.white)
+                .padding(5)
+                .background(.black)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
     }
 }
 
