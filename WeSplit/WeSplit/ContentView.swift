46f57889285425c4585aab93b351b5839a855c45
Syimyk <syimyk.nurdinov@axelkg.com>
2026-05-14 11:18:15 +0600

    do the challenge 1

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -66,6 +66,7 @@ struct ContentView: View {
                 
                 Section("Total amount") {
                     Text(grandTotal, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .background(tipPercentage == 0 ? .red : .clear)
                 }
             }
             .navigationTitle("WeSplit")
