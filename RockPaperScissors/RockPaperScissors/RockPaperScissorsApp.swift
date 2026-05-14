51d8b7b22145b180a125bdd8a25160ecf239d5f8
Syimyk <syimyk.nurdinov@axelkg.com>
2026-05-14 12:39:19 +0600

    add new challenge project

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Syimyk on 14/5/26.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
